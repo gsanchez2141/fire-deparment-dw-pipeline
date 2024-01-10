16c27260c10144348c45b239471faf934021d590
Gabriel Sanchez <gabriel.sanchez@uberall.com>
2024-01-10 18:04:09 -0300

    Infra resources - Docker compose with postgres db and localstack, localstack uses s3 emulating the arrival of data with the generator job. docker-script.sh starts docker compose and loads the data sample file to s3, init.sql holds the different table definitions raw, stage, dimensions and fact

---
+++ bin/s3_generator.sh
@@ -0,0 +1,48 @@
+# Get the directory of the script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check if a valid argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 [start|stop]"
+    exit 1
+fi
+
+# Change to the script's directory
+cd "$SCRIPT_DIR" || exit
+
+# Get the current directory
+CURRENT_DIR="$(pwd)"
+
+# Start block
+if [ "$1" == "start" ]; then
+
+
+    echo "Starting Generator."
+    # Install and Activate raw virtual environment
+    python3 -m venv "$CURRENT_DIR/venv"
+    source "$CURRENT_DIR/venv/bin/activate" && echo "Virtual environment is active."
+
+    # Run S3 messages generator
+    pip install -r "$CURRENT_DIR/../app_s3_generator/requirements.txt"
+    python "$CURRENT_DIR/../app_s3_generator/generator.py"
+
+    # Deactivate & remove virtual environment
+    deactivate && echo "Virtual environment is not active."
+    rm -rf "$CURRENT_DIR/venv"
+
+
+    echo "Starting Fire Departament JOB"
+    # Install and Activate raw virtual environment
+    python3 -m venv "$CURRENT_DIR/venv"
+    source "$CURRENT_DIR/venv/bin/activate" && echo "Virtual environment is active."
+
+    # Run S3 messages generator
+    pip install -r "$CURRENT_DIR/../app/src/requirements.txt"
+    python "$CURRENT_DIR/../app/src/app.py"
+
+    # Deactivate & remove virtual environment
+    deactivate && echo "Virtual environment is not active."
+    rm -rf "$CURRENT_DIR/venv"
+
+fi
+
